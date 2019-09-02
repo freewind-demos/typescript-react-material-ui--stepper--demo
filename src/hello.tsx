d39b4e3fc5371d7e374375aeb8bab627a669295d
Peng Li <peng.li@compass.com>
2019-09-02 15:50:02 +0800

    typescript-react-material-ui--stepper--demo

--- src/hello.tsx
+++ src/hello.tsx
@@ -1,45 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
 
-import {List, ListItem, ListItemText, ListItemIcon, Collapse} from '@material-ui/core'
-import InboxIcon from '@material-ui/icons/Inbox'
-import {ExpandMore, ExpandLess} from '@material-ui/icons'
+export default function MyList() {
+  const [activeStep, setActiveStep] = useState(0)
 
-class MyList extends React.Component {
-  state = {
-    open: false
+  function content(activeStep: number) {
+    switch (activeStep) {
+      case 0 :
+        return <div>Content of step 1</div>
+      case 1:
+        return <div>Content of step 2</div>
+      case 2:
+        return <div>Content of step 3</div>
+    }
   }
 
-  toggleSublist = () => {
-    this.setState({
-      open: !this.state.open
-    })
-  }
+  return <div>
+    <Stepper activeStep={activeStep} alternativeLabel>
+      <Step><StepLabel>Step 1</StepLabel></Step>
+      <Step><StepLabel>Step 2</StepLabel></Step>
+      <Step><StepLabel>Step 3</StepLabel></Step>
+    </Stepper>
+    {content(activeStep)}
+    {
+      activeStep >= 3
+        ? <Button variant='outlined' onClick={() => setActiveStep(0)}>Reset</Button>
+        : <Button variant='outlined' onClick={() => setActiveStep(activeStep + 1)}>Next</Button>
+    }
 
-  render() {
-    return <div>
-      <List>
-        <ListItem button onClick={this.toggleSublist}>
-          <ListItemIcon>
-            <InboxIcon/>
-          </ListItemIcon>
-          <ListItemText primary='Hello'/>
-          {
-            this.state.open ? <ExpandLess/> : <ExpandMore/>
-          }
-        </ListItem>
-        <Collapse in={this.state.open}>
-          <List>
-            <ListItem>
-              <ListItemText primary='typescript'/>
-            </ListItem>
-            <ListItem>
-              <ListItemText primary='material-ui'/>
-            </ListItem>
-          </List>
-        </Collapse>
-      </List>
-    </div>
-  }
+  </div>
 }
-
-export default MyList
